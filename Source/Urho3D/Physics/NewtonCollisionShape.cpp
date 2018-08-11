6a4b7de1b6f01592042f22b9526b4595dd99cb48
TrevorCash <cashtrevor@msn.com>
2018-08-11 15:02:12 -0600

    Newton: remove some debugs

--- Source/Urho3D/Physics/NewtonCollisionShape.cpp
+++ Source/Urho3D/Physics/NewtonCollisionShape.cpp
@@ -452,7 +452,6 @@ namespace Urho3D {
 
             newtonMesh_ = physicsWorld_->GetCreateNewtonMesh(meshKey);
             NewtonMeshBeginBuild(newtonMesh_->mesh);
-            URHO3D_LOGINFO(String(indexCount));
 
 
 
@@ -483,7 +482,6 @@ namespace Urho3D {
                 NewtonMeshAddPoint(newtonMesh_->mesh, v2.x_, v2.y_, v2.z_);
                 NewtonMeshAddPoint(newtonMesh_->mesh, v3.x_, v3.y_, v3.z_);
 
-                URHO3D_LOGINFO("Face: (" + String(v1) + ") | (" + String(v2) + ") | (" + String(v3) + ")");
 
                 NewtonMeshEndFace(newtonMesh_->mesh);
             }
