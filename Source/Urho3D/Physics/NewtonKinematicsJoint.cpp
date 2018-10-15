bcf198969e214544ad39c947579b6f9fe5328a25
TrevorCash <cashtrevor@msn.com>
2018-10-15 12:34:45 -0600

    Newton: Make KinematicsControllerConstraint Iterative.

--- Source/Urho3D/Physics/NewtonKinematicsJoint.cpp
+++ Source/Urho3D/Physics/NewtonKinematicsJoint.cpp
@@ -18,7 +18,7 @@ namespace Urho3D {
 
     KinematicsControllerConstraint::KinematicsControllerConstraint(Context* context) : Constraint(context)
     {
-
+        SetSolveMode(SOLVE_MODE_ITERATIVE);
     }
 
     KinematicsControllerConstraint::~KinematicsControllerConstraint()
