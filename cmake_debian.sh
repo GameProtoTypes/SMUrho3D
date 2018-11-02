e53d80aa0a32641ff48a5e50b0b3566d9deb41b6
TrevorCash <cashtrevor@msn.com>
2018-11-01 19:58:41 -0700

    Trying to get newton to build on linux within urho

--- cmake_debian.sh
+++ cmake_debian.sh
@@ -2,7 +2,8 @@
 
 rm -r -f build
 
-cmake -Bbuild -H. -DURHO3D_RENDERER=OpenGL \
+cmake -Bbuild -H. \
+-DURHO3D_RENDERER=OpenGL \
 -DURHO3D_ENABLE_ALL=ON \
 -DBUILD_SHARED_LIBS=ON \
 -DURHO3D_CSHARP=OFF \
