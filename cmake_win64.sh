3c5d8592a2b11ad809c88e4a7c5b109dc52630ad
TrevorCash <cashtrevor@msn.com>
2018-11-01 17:55:18 -0700

    Changes On Linux

--- cmake_win64.sh
+++ cmake_win64.sh
@@ -6,9 +6,3 @@ cmake -Bbuild -H. -DURHO3D_RENDERER=OpenGL \
 -DURHO3D_ENABLE_ALL=ON \
 -DBUILD_SHARED_LIBS=ON \
 -DURHO3D_CSHARP=OFF \
-# \
-# -DLLVM_VERSION_EXPLICIT=5.0.1 \
-# -DLIBCLANG_LIBRARY="C:/Program Files/LLVM/lib/libclang.lib" \
-# -DLIBCLANG_INCLUDE_DIR="C:/Program Files/LLVM/include" \
-# -DLIBCLANG_SYSTEM_INCLUDE_DIR="C:/Program Files/LLVM/lib/clang/5.0.1/include" \
-# -DCLANG_BINARY="C:/Program Files/LLVM/bin/clang++.exe"
